315881f089147bd2a8913238673787d5c1a43917
Matheus Basy <contato.basy@gmail.com>
2025-09-02 17:38:59 -0300

    Criado limitadores e verificadores nas funções, adicionado a função de calcular 4 financiamentos ao mesmo tempo

--- src/Interfaces/InterfaceUsuario.java
+++ src/Interfaces/InterfaceUsuario.java
@@ -10,18 +10,42 @@ public class InterfaceUsuario {
 
     Scanner sc = new Scanner(System.in);
 
-    public double pedirValorImovel() {
-        System.out.println("Qual o valor do imovel?");
-        return fin1.valorImovel = sc.nextDouble();
+    public double pedirValorImovel(int i) {
+        System.out.println("Qual o valor do imóvel " + i + "?" );
+        fin1.valorImovel = sc.nextDouble();
+        if (fin1.valorImovel <= 0) {
+            System.out.println("O valor do imóvel não pode ser menor que zero.");
+            return pedirValorImovel(i);
+        }
+
+        return fin1.valorImovel;
     }
 
-    public int pedirPrazoFinanciamento() {
-        System.out.println("Qual o prazo do financiamento? (Em Anos)");
-        return fin1.prazoFinanciamento = sc.nextInt();
+    public int pedirPrazoFinanciamento(int i) {
+        System.out.println("Qual o prazo do financiamento " + i +"? (Em Anos)");
+        fin1.prazoFinanciamento = sc.nextInt();
+
+        if (fin1.prazoFinanciamento <= 0) {
+            System.out.println("O prazo do financiamento não pode ser menor que zero anos.");
+            return pedirPrazoFinanciamento(i);
+        } else if (fin1.prazoFinanciamento > 30) {
+            System.out.println("O prazo do financiamento não pode ser maior que 30 anos.");
+            return pedirPrazoFinanciamento(i);
+        }
+
+        return fin1.prazoFinanciamento;
     }
 
     public double pedirTaxaJurosAnual() {
-        System.out.println("Qual a taxa de juros anual?");
-        return fin1.taxaJurosAnual = sc.nextDouble();
+        System.out.println("Qual a taxa de juros anual? (Máximo de 20%)");
+        fin1.taxaJurosAnual = sc.nextDouble();
+        if (fin1.taxaJurosAnual <= 1) {
+            System.out.println("A taxa de juros anual não pode ser negativa.");
+            return pedirTaxaJurosAnual();
+        } else if (fin1.taxaJurosAnual > 20) {
+            System.out.println("A taxa de juros anual não pode ser maior que 20%.");
+            return pedirTaxaJurosAnual();
+        }
+        return fin1.taxaJurosAnual;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,38 @@
-import java.util.Scanner;
 import Interfaces.InterfaceUsuario;
 import Financiamentos.Financiamento;
+import java.util.ArrayList;
+
 
 public class Main {
 
     public static void main(String[] args) {
         InterfaceUsuario ui = new InterfaceUsuario();
-        Financiamento fnc = new Financiamento(1,1,1);
+        Financiamento fnc = new Financiamento(1, 1, 1);
+        ArrayList<String> listaFinanciamentos = new ArrayList<>();
 
         System.out.println("Sistema de Financiamento bancario!");
 
-
-        fnc.valorImovel = ui.pedirValorImovel();
-
-
-        fnc.prazoFinanciamento = ui.pedirPrazoFinanciamento();
-
-
+        // Solicitar taxa de juros
         fnc.taxaJurosAnual = ui.pedirTaxaJurosAnual();
 
-        System.out.println("O valor a ser pago mensalmente é de R$ " + fnc.calcularPagamentoMensal());
-        System.out.println("O valor total a ser pago é de R$ " + fnc.calcularTotalPagamento());
+
+        for (int i = 1; i < 5; i++) {
+            // Solicitar valor do imovel
+            fnc.valorImovel = ui.pedirValorImovel(i);
+            // Solicitar prazo de financiamento
+            fnc.prazoFinanciamento = ui.pedirPrazoFinanciamento(i);
+            listaFinanciamentos.add("Financiamento "+ i + " - valor do imóvel: R$ " + String.format("%.2f", fnc.valorImovel) + ", valor do financiamento: R$ " + String.format("%.2f", fnc.calcularTotalPagamento()) + "." );
+        }
+        for (int i = 0; i < 4; i++) {
+            System.out.println(listaFinanciamentos.get(i));
+        }
+
+        System.out.println("Taxa de juros anual de: " + String.format("%.2f",fnc.taxaJurosAnual) + "%");
+
+
+        // Resultado final
+        //System.out.println("O valor a ser pago mensalmente é de R$ " + String.format("%.2f", fnc.calcularPagamentoMensal()));
+        //System.out.println("O valor total a ser pago é de R$ " + String.format("%.2f",fnc.calcularTotalPagamento()));
 
     }
 }
